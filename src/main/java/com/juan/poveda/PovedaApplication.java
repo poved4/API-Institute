62f8bb215b9d19f9baffa6b52143a51c926f2e6b
juan <poved4@gmail.com>
2026-04-15 09:15:49 -0500

    first commit

---
+++ src/main/java/com/juan/poveda/PovedaApplication.java
@@ -0,0 +1,13 @@
+package com.juan.poveda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PovedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PovedaApplication.class, args);
+	}
+
+}
