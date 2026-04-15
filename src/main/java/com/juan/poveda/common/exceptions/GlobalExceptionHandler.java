68fddd7394f090967916072605ff382cff9b23fc
juan <poved4@gmail.com>
2026-04-15 17:28:40 -0500

    feat(exception): implement global exception handling and custom errors
    - Added GlobalExceptionHandler using @RestControllerAdvice. - Handled common Spring Boot exceptions (404, 400, 500).

---
+++ src/main/java/com/juan/poveda/common/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.juan.poveda.common.exceptions;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNotFound(EntityNotFoundException ex) {
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(Map.of("error", ex.getMessage()));
+    }
+
+}
